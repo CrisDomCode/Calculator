65a1f38ae5703310666cb6a713be16fc9fba3f78
CrisDomCode <cristina.dominguez.cd@gmail.com>
2024-11-26 00:14:52 +0100

    ADD operate()

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log('JS activated')
+// BASIC FUNCTIONS
+  function operate(a, operator, b) {
+    switch (operator) {
+      case '+':
+        return a + b
+        break;
+      case '-':
+        return a - b
+        break;
+      case '*':
+        return a * b
+        break;
+      case '/':
+        return a / b
+        break;
+      default:
+        break;
+    }
+  }
+
+let a = Number(prompt("First number ?"))
+let operator = prompt("Operator ?")
+let b = Number(prompt("Second number ?"))
+
+alert(operate(a, operator, b))
+
+// Ajouter Toggle CSS https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29
